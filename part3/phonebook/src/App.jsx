e6634c42d58e0964e521ac6831ac7312289d2da8
Shehzad Khuwaja <shehzadkhuwaja@gmail.com>
2023-12-08 19:54:30 +0500

    2.16 and 2.17 completed

--- part3/phonebook/src/App.jsx
+++ part3/phonebook/src/App.jsx
@@ -4,7 +4,45 @@ import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import personService from './services/note'
 
+const Notification = ({message, type}) => {
 
+  const successNotificationStyle = {
+    color: 'green',
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  }
+
+  const errorNotificiationStyle = {
+    color: 'red',
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  }
+
+  if (message === null) {
+    return null
+  }
+
+  if (type === 'success') {
+    return (
+      <div style={successNotificationStyle}>{message}</div>
+    )
+  }
+  else if (type === 'failure') {
+    return (
+      <div style={errorNotificiationStyle}>{message}</div>
+    )
+  }
+
+  
+}
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
@@ -13,6 +51,10 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
 
   const [nameSearch, setNewSearch] = useState('')
+
+  const [successMessage, setSuccessMessage] = useState(null)
+
+  const [errorMessage, setErrorMessage] = useState(null)
 
   const filteredPhoneBook = persons.filter((person) => person.name.toUpperCase().includes(nameSearch.toUpperCase()))
 
@@ -47,12 +89,19 @@ const App = () => {
       .update(objectToUpdate.id, updatedObject)
       .then(response => {
         setPersons(persons.map(person => person.id !== objectToUpdate.id ? person : response.data ))
-        setNewName('') 
-        setNewNumber('')
+
+        setSuccessMessage(`${newName} number has been updated`)
+        setTimeout(() => setSuccessMessage(null), 5000)
+      })
+      .catch(error => {
+        setErrorMessage(`Information of ${objectToUpdate.name} has already been removed from server`)
+        setTimeout(() => setErrorMessage(null), 5000)
       })
 
-      return
+      setNewName('') 
+      setNewNumber('')
 
+      return
     }
   
     const newNameobject = {
@@ -67,6 +116,9 @@ const App = () => {
       setNewName('') 
       setNewNumber('')
     })
+
+    setSuccessMessage(`Added ${newName}`)
+    setTimeout(() => setSuccessMessage(null), 5000)
 
   }
 
@@ -85,6 +137,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <Notification message={successMessage} type="success" />
+
+      <Notification message={errorMessage} type="failure" />
 
       <Filter nameSearch={nameSearch} handleNameSearch={handleNameSearch} />
 
