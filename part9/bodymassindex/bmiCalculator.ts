19f1a3c739f5502a35c1d554a74e66b6b3e76377
ShehzadKhuwaja <103369085+ShehzadKhuwaja@users.noreply.github.com>
2024-07-04 20:43:08 +0500

    incomplete work

--- part9/bodymassindex/bmiCalculator.ts
+++ part9/bodymassindex/bmiCalculator.ts
@@ -1,15 +1,47 @@
-const calculateBmi = (height: number, weight: number): string => {
-    const bmi = (weight * 10000) / height ** 2
+interface Bmi {
+    height: number;
+    weight: number;
+}
 
-    if (18.5 <= bmi && bmi <= 24.5) {
-        return 'Normal (healthy weight)'
-    } else if (bmi < 18.5) {
-        return 'underweight'
-    } else if (24.5 < bmi && bmi <= 29.9) {
-        return 'overweight'
-    } else {
-        return 'obese'
+
+const parseArguments = (args: string[]): Bmi => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            height: Number(args[2]),
+            weight: Number(args[3])
+        }
+    }
+    else {
+        throw new Error('Provided values were not numbers!');
     }
 }
 
-console.log(calculateBmi(180, 74))
+const calculateBmi = (height: number, weight: number): string => {
+    const bmi = (weight * 10000) / height ** 2;
+
+    if (18.5 <= bmi && bmi <= 24.5) {
+        return 'Normal (healthy weight)';
+    } else if (bmi < 18.5) {
+        return 'underweight';
+    } else if (24.5 < bmi && bmi <= 29.9) {
+        return 'overweight';
+    } else {
+        return 'obese';
+    }
+}
+
+//console.log(calculateBmi(180, 74));
+
+try {
+    const { height, weight } = parseArguments(process.argv);
+    calculateBmi(height, weight);
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.';
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
--- part9/bodymassindex/exerciseCalculator.ts
+++ part9/bodymassindex/exerciseCalculator.ts
@@ -1,32 +1,32 @@
 interface Achievement {
-    periodLength: number,
-    trainingDays: number,
-    success: boolean,
-    rating: number,
-    ratingDescription: string,
-    target: number,
-    average: number
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number;
 }
 
 const calculateExercises = (days: number[], target: number): Achievement => {
-    const periodLength = days.length
-    const trainingDays = days.filter(hour => hour !== 0).length
-    const sum = days.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
-    const average = sum / periodLength
-    const success = average >= target
+    const periodLength = days.length;
+    const trainingDays = days.filter(hour => hour !== 0).length;
+    const sum = days.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    const average = sum / periodLength;
+    const success = average >= target;
 
-    let rating = null
-    let ratingDescription = null
+    let rating = null;
+    let ratingDescription = null;
 
     if (success) {
-        rating = 3
-        ratingDescription = 'Excellent'
+        rating = 3;
+        ratingDescription = 'Excellent';
     } else if ((target - average) <= 0.5) {
-        rating = 2
-        ratingDescription = 'not too bad but could be better'
+        rating = 2;
+        ratingDescription = 'not too bad but could be better';
     } else {
-        rating = 1
-        ratingDescription = 'need improvement'
+        rating = 1;
+        ratingDescription = 'need improvement';
     }
 
     return {
@@ -40,4 +40,4 @@ const calculateExercises = (days: number[], target: number): Achievement => {
     }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
